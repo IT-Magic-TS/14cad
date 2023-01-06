1858f3d122066c7503df3b995b33872be60366dc
Peter Krizovnik <info@itmagic.dev>
2023-01-06 15:39:03 +0000

    SW 1

--- sw.js
+++ sw.js
@@ -30,22 +30,6 @@ self.addEventListener('install', function(event) {
           '/images/A5-sm.webp',
           '/images/A5.png',
           '/images/A5.webp',
-          '/images/android-1.png',
-          '/images/android-1.webp',
-          '/images/android-2.png',
-          '/images/android-2.webp',
-          '/images/android-3.png',
-          '/images/android-3.webp',
-          '/images/logo.webp',
-          '/images/logo.png',
-          '/images/facebook-icon.png',
-          '/images/facebook-icon.webp',
-          '/images/instagram.png',
-          '/images/instagram.webp',
-          '/images/14cad-advert-1.png',
-          '/images/14cad-advert-1.webp',
-          '/images/14cad-advert-2.png',
-          '/images/14cad-advert-2.webp'
         ])
         // cache.add('/')
         // cache.add('/index.html')
