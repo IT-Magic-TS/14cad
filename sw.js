41f87856d173424ac046821b4da3d9a724a34457
Peter Krizovnik <info@itmagic.dev>
2023-01-06 13:30:09 +0000

    SW 1

--- sw.js
+++ sw.js
@@ -4,9 +4,22 @@ self.addEventListener('install', function(event) {
     caches.open('static')
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell...')
-        cache.add('/')
-        cache.add('/index.html')
-        cache.add('/js/promise.js')
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/icons/favicon.png',
+          '/https://fonts.googleapis.com',
+          '/https://fonts.gstatic.com',
+          '/https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap',
+          '/css/style.css',
+          '/js/promise.js',
+          '/js/fetch.js',
+          '/js/script.js',
+          '/js/calculations.js',
+          '/js/project-cost.js'
+        ])
+        // cache.add('/')
+        // cache.add('/index.html')
       })
     )
 })
